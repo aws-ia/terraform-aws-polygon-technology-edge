1dbe419bd993e53a16d41f8c70876c81b4dbe67e
ZeljkoBenovic <sakal87@gmail.com>
2022-08-12 20:50:51 +0200

    removed legacy bastion output variable

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "jsonrpc_dns_name" {
   value       = module.alb.dns_name
   description = "The dns name for the JSON-RPC API"
 }
-
-#output "bastion_instance_public_ip" {
-#  value = module.bastion_instance.bastion_eip
-#  description = "The public ip address of the bastion instance"
-#}
