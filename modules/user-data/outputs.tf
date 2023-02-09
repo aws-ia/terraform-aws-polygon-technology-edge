239b548f5828e71f3d951fea8a70cfa5efdf6658
aiell0 <laiello@amazon.com>
2023-02-09 11:41:30 -0500

    fix: remove deprecated templates module

--- modules/user-data/outputs.tf
+++ modules/user-data/outputs.tf
@@ -1,4 +1,4 @@
 output "polygon_edge_node" {
-  value       = data.template_cloudinit_config.polygon_edge.rendered
+  value       = data.cloudinit_config.polygon_edge.rendered
   description = "Base64 rendered polygon edge node user-init data"
 }
